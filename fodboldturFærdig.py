b0fb0bf2c99e168beb9bdd1056de7f8c45647629
Askemanden <askevandsoaremark@gmail.com>
2025-10-25 16:22:23 +0200

    done

--- fodboldturFærdig.py
+++ fodboldturFærdig.py
@@ -5,10 +5,15 @@
     4.    Udskrive liste over de tre medlemmer, der mangler at betale mest.
 """
 import pickle
+from math import inf
 
 filename = 'betalinger.pk'
 
-fodboldtur ={}
+TOTAL_BELØB = 4500
+
+infile = open(filename,'rb')
+fodboldtur : dict = pickle.load(infile)
+infile.close()
 
 def afslut():
     outfile = open(filename, 'wb')
@@ -17,22 +22,65 @@ def afslut():
     print("Programmet er afsluttet!")
 
 def printliste():
-    for item in fodboldtur.items():
-        print(item[0])
-    menu()
+    BELØB_PR_PERSON = TOTAL_BELØB/len(fodboldtur)
+    for navn, beløb in fodboldtur.items():
+        print(navn)
+        print(" indbetalt", beløb)
+        if beløb >= BELØB_PR_PERSON:
+            print(" mangler ingen inbetalinger")
+        else:
+            print(" mangler at indbetale", BELØB_PR_PERSON - beløb)
+    
+    
 
-def menu():
-    print("MENU")
-    print("1: Print liste")
-    print("2: Afslut program")
-    valg = input("Indtast dit valg:")
-    if (valg == '1'):
-        printliste()
-    if (valg == '2'):
-        afslut()
+def indbetal():
+    navn = input("Indtast navn af indbetalende person ")
+    if (not navn in fodboldtur.keys()):
+        print("Navn er ikke med\n")
+        return
+    betaling = input("Indtast beløb ")
+    try:
+        betaling = int(betaling)
+    except Exception as e:
+        print("Invalidt input ",e)
+        return
+    fodboldtur[navn] += betaling
+    
 
-infile = open(filename,'rb')
-fodboldtur = pickle.load(infile)
-infile.close()
-menu()
+def mindstBetalende():
+    personer = {}
+    for person, betalt in fodboldtur.items():
+        if len(personer) < 3:
+            personer[person] = betalt
+            continue
 
+        maxBeløb = ["", -float("inf")]
+        for navn, beløb in personer.items():
+            if beløb > maxBeløb[1]:
+                maxBeløb = [navn, beløb]
+
+        if betalt < maxBeløb[1]:
+            personer.pop(maxBeløb[0])
+            personer[person] = betalt
+
+    print(personer)
+
+
+
+def loop():
+    running = True
+    while running:
+        print("MENU\n1: Print liste\n2: Indbetal\n3: Mindst betalende medlemmer\nE: End og gem")
+        valg = input("Indtast dit valg: ")
+        if (valg == '1'):
+            printliste()
+        elif (valg == "2"):
+            indbetal()
+        elif (valg == "3"):
+            mindstBetalende()
+        elif (valg == 'E'):
+            afslut()
+            running = False
+
+if __name__ == "__main__":
+    loop()
